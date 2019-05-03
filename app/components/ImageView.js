cec25ef3229b2ca8f99f4a1710dbd80d764d4961
Barbara Verburg <barb@zenzillo.com>
2019-05-03 11:53:33 -0400

    Initalize app

---
+++ app/components/ImageView.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+
+export class BigImage extends React.Component {
+	render() {
+		return (
+		   <Image src={this.props.src} rounded fluid />
+		);
+	}
+};
+
+export class CatChoices extends React.Component {
+	render() {
+		const catList = this.props.cats;
+		const catBlocks = catList.map((cat) =>
+			  <Card>
+			    <Card.Img variant="top" src={cat.src} />
+			    <Card.Body>
+			      <Card.Title>{cat.name}</Card.Title>
+			      <Card.Text>{cat.description}</Card.Text>
+			      <Button variant="primary">Select <Badge variant="light">{cat.clicks}</Badge></Button>
+			    </Card.Body>
+			  </Card>
+			);
+
+		return (
+			<CardDeck>
+				{catBlocks}
+			</CardDeck>
+		);
+	}
+};
---
+++ app/index.js
@@ -0,0 +1,67 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+require('./index.css');
+
+import { BigImage, CatChoices } from './components/ImageView';
+
+const cats = [
+	{
+		src: 'https://images.pexels.com/photos/2061057/pexels-photo-2061057.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		name: 'Penelope',
+		description: 'Staring contest champion.',
+		clicks: 0
+	},
+	{
+		src: 'https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+		name: 'Pickles',
+		description: 'The excitement from watching Jeopardy is just too much.',
+		clicks: 0
+	},
+	{
+		src: 'https://images.pexels.com/photos/399647/pexels-photo-399647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		name: 'Tiger',
+		description: 'Waffles!!!!!!!!',
+		clicks: 0
+	}
+]
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			cats: cats
+		}
+	}
+
+	render() {
+		return (
+		    <div>
+			  <Container>
+				  <Row>
+				    <Col><h1>Cat Clicker</h1></Col>
+				  </Row>
+				  <Row>
+				    <Col>
+				    	<BigImage
+				    		src={this.state.cats[0].src}
+				    	/>
+				    	Clicks: {this.state.cats[0].clicks}
+				    </Col>
+				  </Row>
+				  <Row>
+				 	<Col><CatChoices cats={this.state.cats} /></Col>
+				  </Row>
+				</Container>
+		    </div>
+		)
+	}
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
